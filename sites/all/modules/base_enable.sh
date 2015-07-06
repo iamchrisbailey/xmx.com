68fbd1a50ceb3f453687a27a98bb20608f58b7ea
iamchrisbailey <iamchrisbailey@gmail.com>
2015-07-06 13:57:46 -0400

    added strongarm module

---
+++ sites/all/modules/base_enable.sh
@@ -0,0 +1,10 @@
+#script for enabling modules that are not features
+
+drush en -y admin_menu, admin_menu_toolbar
+drush en -y auto_nodetitle
+drush en -y ctools
+drush en -y features
+drush en -y pathauto
+drush en -y strongarm
+drush en -y token
+drush en -y views, views_ui
